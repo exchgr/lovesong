95cd545b0ae8566a16affcf06e3ab9f468ebaa60
Moiri Gamboni <moiri.gamboni@nyu.edu>
2013-02-22 18:46:16 +0400

    commit3

--- mapper.js
+++ mapper.js
@@ -1,51 +1,52 @@
 var landmarks = [
 	{
 		name: "landmark1",
-		latlong: new google.maps.LatLng(37.77, -122.44),
-		importance: 1,
+		latlong: new google.maps.LatLng(37.77, -122.45),
+		importance: 1
 	},
 	{
 		name: "landmark2",
-		latlong: new google.maps.LatLng(37.78, -122.45),
-		importance: 0,
-	};
+		latlong: new google.maps.LatLng(37.76, -122.44),
+		importance: 0
+	}
 ];
 
-var mapper = {
-	// given a DOM Object, destination bject, and an array of landmark objects
-	// inserts a map with the destination and landmark pins to that DOM
-	var map;
-	var markers = [];
-	var img_array = [
+var map;
+var markers = [];
+var img_array = [
 		{
 	    	url: 'images/marker0.png',
 	    	size: new google.maps.Size(20, 32),
 	    	origin: new google.maps.Point(0,0),
-	    	anchor: new google.maps.Point(0, 32)
+	    	anchor: new google.maps.Point(10, 32)
 		},
 		{
 	    	url: 'images/marker1.png',
 	    	size: new google.maps.Size(20, 32),
 	    	origin: new google.maps.Point(0,0),
-	    	anchor: new google.maps.Point(0, 32)
+	    	anchor: new google.maps.Point(10, 32)
 		},	
 		{
 	    	url: 'images/marker2.png',
 	    	size: new google.maps.Size(20, 32),
 	    	origin: new google.maps.Point(0,0),
-	    	anchor: new google.maps.Point(0, 32)
+	    	anchor: new google.maps.Point(10, 32)
 		}							
 	];
+
+var mapper = {
+	// given a DOM Object, destination bject, and an array of landmark objects
+	// inserts a map with the destination and landmark pins to that DOM
 	
 	addMarker: function(location, importance, name) {
 		marker = new google.maps.Marker({
 			position: location,
-			map: map,
 			icon: img_array[importance],
+			map: map,
 			title: name
 		});
 		markers.push(marker);
-	}
+	},
 
 	clearOverlays: function() {
 	  if (markers) {
@@ -53,7 +54,7 @@ var mapper = {
 		  markers[i].setMap(null);
 		}
 	  }
-	}
+	},
 
 	showOverlays: function() {
 	  if (markers) {
@@ -61,7 +62,7 @@ var mapper = {
 		  markers[i].setMap(map);
 		}
 	  }
-	}
+	},
 
 	deleteOverlays: function() {
 	  if (markers) {
@@ -70,7 +71,7 @@ var mapper = {
 		}
 		markers.length = 0;
 	  }
-	}
+	},
 	
 	map: function(obj, landmarks) {
 		var center = new google.maps.LatLng(37.7699298, -122.4469157);
@@ -79,9 +80,10 @@ var mapper = {
 			center: center,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
-		map =  new google.maps.Map(obj, mapOptions);
-		for (lm in landmarks) {
-			addMarker(lm.latlong, lm.importance, lm.name);
+		map = new google.maps.Map(obj, mapOptions);
+		for (i in landmarks) {
+			var lm = landmarks[i];
+			mapper.addMarker(lm.latlong, lm.importance, lm.name);
 		}
 	}
-}
+};
