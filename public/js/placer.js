6e5b745eb410933479603d3e2009a58b2c9ed3d4
Morgante Pell <morgante.pell@morgante.net>
2013-02-22 16:38:36 +0400

    abstract

---
+++ public/js/placer.js
@@ -0,0 +1,21 @@
+// Landmark object
+// 
+// Properties:
+//	* name
+//	* latitude
+//	* longitude
+// 	* color
+
+var placer = {
+	// given a latlong( lat, long), and precision
+	// 	precision: 0 (low - general vicinity) or 1 (high - for directions)
+	// returns an array of landmark objects — ranked by "landmarkiness" and "proximity"
+	getLandmarks: function( latlong, precision ) {
+		
+	}
+	
+	// given a latilong( lat, long), and precision, returns the "best" landmark from getLandmarks
+	getLandmark: function( lat, long, precision ) {
+		
+	}
+}
