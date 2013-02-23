4a96cd55b4e21a05ce06d0023cd1a31256a2c2f9
Morgante Pell <morgante.pell@morgante.net>
2013-02-23 10:35:49 +0400

    simple javascript server

--- public/js/main.js
+++ public/js/main.js
@@ -29,39 +29,6 @@ var main = {
 		
 		searchBox.bindTo('bounds', mapper.map);
 		
-		// google.maps.event.addListener(searchBox, 'places_changed', function() {
-		// 	var places = searchBox.getPlaces();
-		// 
-		// 	    for (var i = 0, marker; marker = markers[i]; i++) {
-		// 	      marker.setMap(null);
-		// 	    }
-		// 
-		// 	    markers = [];
-		// 	    var bounds = new google.maps.LatLngBounds();
-		// 	    for (var i = 0, place; place = places[i]; i++) {
-		// 	      var image = {
-		// 	        url: place.icon,
-		// 	        size: new google.maps.Size(71, 71),
-		// 	        origin: new google.maps.Point(0, 0),
-		// 	        anchor: new google.maps.Point(17, 34),
-		// 	        scaledSize: new google.maps.Size(25, 25)
-		// 	      };
-		// 
-		// 	      var marker = new google.maps.Marker({
-		// 	        map: map,
-		// 	        icon: image,
-		// 	        title: place.name,
-		// 	        position: place.geometry.location
-		// 	      });
-		// 
-		// 	      markers.push(marker);
-		// 
-		// 	      bounds.extend(place.geometry.location);
-		// 	    }
-		// 
-		// 	    map.fitBounds(bounds);
-		// 	  });
-		
 		if (navigator.geolocation) {
 			$('.nav-current').click( function( ev) {
 				navigator.geolocation.getCurrentPosition( function( position ) {
