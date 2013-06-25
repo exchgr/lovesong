9bfbe73ce5aeeb981d4c74b96a0779ed56869d72
Morgante Pell <morgante.pell@morgante.net>
2013-06-26 03:41:14 +0400

    include a sample model

--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,11 @@
+var Sample = require('../models/sample')
+
 exports.index = function( req, res ) {
+	
+	Sample.create({ "first": "Bob", "last": "Sam"}, function( err, smpl ) {
+		console.log( smpl.getName() );
+	});
+	
 	res.render("index", {
 	});
 }
