d573102371209da0fa20fab4882b168a85568c6e
Morgante Pell <morgante.pell@morgante.net>
2013-08-03 14:35:22 -0400

    facebook graph, yay

--- models/user.js
+++ models/user.js
@@ -6,11 +6,14 @@ var _ = require('underscore');
 schema =  new mongoose.Schema({
 	username: String,
 	joined:   {type: Date, default: Date.now},
-	externals: {type: mongoose.Schema.Types.Mixed, default: {}}
+	externals: {type: mongoose.Schema.Types.Mixed, default: {}},
+	_artists: {
+		facebook: {type: mongoose.Schema.Types.Mixed, default: []}
+	}
 });
 
 // schema.virtual('url').get(function () {
-//   return 'http://caus.io/' + this.username;
+  // return 'http://caus.io/' + this.username;
 // });
 
 schema.virtual('name').get(function () {
@@ -21,6 +24,23 @@ schema.virtual('name').get(function () {
 	{
 		return this.username;
 	}
+});
+
+schema.virtual('fbToken').get(function () {
+	return this.externals.facebook.accessToken;
+});
+
+schema.virtual('artists').get(function () {
+	artists = [];
+	
+	_.each(this._artists.facebook, function(artist) {
+		artists.push({
+			'name': artist.name,
+			'fbID': artist.id
+		})
+	})
+	
+	return artists;
 });
 
 // schema.virtual('givenName').get(function () {
