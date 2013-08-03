be111aceeea00fc860c96441516674f4bc330097
Morgante Pell <morgante.pell@morgante.net>
2013-08-03 13:23:28 -0400

    ready to go

--- server.js
+++ server.js
@@ -20,16 +20,19 @@ db.once('open', function callback() {
 var app = express();
 // configure Express
 app.configure(function() {
+	app.set('port', process.env.PORT || 5000);
 	app.set('views', __dirname + '/views');
-	app.set('view engine', 'ejs');
-	app.engine('ejs', require('ejs-locals'));
-
-	app.use(express.logger());
-	app.use(express.cookieParser());
+	app.set('view engine', 'jade');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
-	app.use(express.session({ secret: process.env.SECRET }));
+	app.use(express.cookieParser( process.env.SECRET ));
+	app.use(express.session({ key: 'lovesong.sess', secret: process.env.SECRET }));
+	// app.use(passport.initialize());
+	// app.use(passport.session());
 	app.use(app.router);
+	app.use(require('stylus').middleware(__dirname + '/public'));
 	app.use(express.static(__dirname + '/public'));
 });
 
