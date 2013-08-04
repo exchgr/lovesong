641e5c9abd23187f34c1a886812257ccc2cdb1f8
Dan Mundy <drm.mundy@gmail.com>
2013-08-03 22:35:54 -0400

    SPACES

--- public/scripts/app/main.js
+++ public/scripts/app/main.js
@@ -1,15 +1,15 @@
 define([
-	'jquery'
+  'jquery'
 ], function ($) {
-	if ($('.navbar.loggedin').length > 0) {		
-		// load the full app
-		require(["app/views/dash"],
-			function(Dashboard) {
-				new Dashboard;
-			}
-		);
-	} else {
-		// keep the landing page lightweight; ie. don't load Backbone, etc.
-		$('.vert-center').css('margin-top', -$('.vert-center').outerHeight()/2 + 'px');
-	}
+  if ($('.navbar.loggedin').length > 0) {
+    // load the full app
+    require(["app/views/dash"],
+            function(Dashboard) {
+              new Dashboard;
+            }
+           );
+  } else {
+    // keep the landing page lightweight; ie. don't load Backbone, etc.
+    $('.vert-center').css('margin-top', -$('.vert-center').outerHeight()/2 + 'px');
+  }
 });
