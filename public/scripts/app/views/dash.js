4cf6e58ad40444ea7c936a4f50d2baef5fba35fc
Dan Mundy <drm.mundy@gmail.com>
2013-08-04 01:00:37 -0400

    Doesn't show modal on page load
    Also gg=G shiftwidth=2

--- public/scripts/app/views/dash.js
+++ public/scripts/app/views/dash.js
@@ -1,46 +1,48 @@
 define([
-	'jquery',
-	'underscore',
-    'backbone',
-    'app/models/profile',
-    'app/views/match'
+  'jquery',
+  'underscore',
+  'backbone',
+  'app/models/profile',
+  'app/views/match'
 ], function ($, _, Backbone, Profiles, Match) {
-    
-	// The Dashboard View
-	// ---------------
-	
-    profiles = Profiles;
 
-	var Dashboard = Backbone.View.extend({
-		el: '#content',
+  // The Dashboard View
+  // ---------------
 
-		initialize: function () {
+  profiles = Profiles;
 
-			// this.listenTo(app.todos, 'reset', this.refresh);
-            this.listenTo(profiles, 'reset', this.setMatch);
-			
-			// fetch default profiles
-			profiles.fetch({reset: true});
-			
-			// init the modal
-		    $('#match').modal({});
-			
-		},
-        
-        setMatch: function () {
-            var profile = profiles.at(0);
-            
-            var view = new Match({model: profile});
-            
-            $('#loading').slideUp();
-            $('#profile-template').slideDown();
-            
-            $('#profile-template').html(view.render().el);
-        },
+  var Dashboard = Backbone.View.extend({
+    el: '#content',
 
-	});
-		
-		// console.log( Dashboard );
-		
-		return Dashboard;
+    initialize: function () {
+
+      // this.listenTo(app.todos, 'reset', this.refresh);
+      this.listenTo(profiles, 'reset', this.setMatch);
+
+      // fetch default profiles
+      profiles.fetch({reset: true});
+
+      // init the modal
+      $('#match').modal({
+        show: false
+      });
+
+    },
+
+    setMatch: function () {
+      var profile = profiles.at(0);
+
+      var view = new Match({model: profile});
+
+      $('#loading').slideUp();
+      $('#profile-template').slideDown();
+
+      $('#profile-template').html(view.render().el);
+    },
+
+  });
+
+  // console.log( Dashboard );
+
+  return Dashboard;
 });
